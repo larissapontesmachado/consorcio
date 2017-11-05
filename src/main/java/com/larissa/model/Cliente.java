466efd8a1dc7ae09d8d903ac1c453b8f4378d7de
larissa pontes <larissapontesm@gmail.com>
2017-11-05 16:36:55 -0200

    meu primeiro commit

---
+++ src/main/java/com/larissa/model/Cliente.java
@@ -0,0 +1,116 @@
+package com.larissa.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private long id;
+	@Column(length = 60, nullable = false)
+	private String nome;
+	private String cpf;
+	private String rg;
+	private String email;
+	private String endereco;
+	private String celular;
+	
+	
+	
+	
+	
+	public Cliente() {
+		
+	}
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+
+	
+
+
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/larissa/util/CriaTabelas.java
@@ -0,0 +1,10 @@
+package com.larissa.util;
+
+import javax.persistence.Persistence;
+
+public class CriaTabelas {
+	public static void main (String[] args){
+		Persistence.createEntityManagerFactory("consorcio");
+	}
+
+}
